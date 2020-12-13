07d5cd2c6dab09a5b23276ab240e51ed63d0f80b
UgurCanGulec <gulecugurcan@gmail.com>
2020-12-13 19:08:21 +0300

    SecondController was added and views were packaged

--- src/main/java/com/j4v4code/controller/SecondController.java
+++ src/main/java/com/j4v4code/controller/SecondController.java
@@ -4,13 +4,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-
 @Controller
-@RequestMapping("/home")
-public class HomeController {
-    @GetMapping("/home")
-    public String home(){
-        return "views/home";
-    }
+@RequestMapping("/second")
+public class SecondController {
 
+    @GetMapping("/second")
+    public String secondMethod() {
+        return "views/second";
+    }
 }
