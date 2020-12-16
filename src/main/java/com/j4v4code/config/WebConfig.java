0a497014791ce69996d9d5a93a11f1134c92484c
UgurCanGulec <gulecugurcan@gmail.com>
2020-12-16 18:11:58 +0300

    Cookie settings were added

--- src/main/java/com/j4v4code/config/WebConfig.java
+++ src/main/java/com/j4v4code/config/WebConfig.java
@@ -3,10 +3,20 @@ package com.j4v4code.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 
 @Configuration
 @EnableWebMvc
@@ -15,7 +25,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Bean
     public InternalResourceViewResolver viewResolver() {
         InternalResourceViewResolver internalResourceViewResolver = new InternalResourceViewResolver();
-        internalResourceViewResolver.setPrefix("/");
+        internalResourceViewResolver.setPrefix("/WEB-INF/views/");
         internalResourceViewResolver.setSuffix(".jsp");
         return internalResourceViewResolver;
     }
@@ -24,4 +34,53 @@ public class WebConfig implements WebMvcConfigurer {
     public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
         configurer.enable();
     }
+
+    /*
+     *Response Karakter Kodlaması İçin
+     * HTTP Response'un da Http Requestte olduğu  gibi UTF-8 karakter kodlamasına göre kodlanması gerekmektedir.
+     */
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        StringHttpMessageConverter stringConverter = new StringHttpMessageConverter();
+
+        List<MediaType> mediaTypeList = new ArrayList<>();
+        mediaTypeList.add(new MediaType("text", "plain", Charset.forName("UTF-8")));
+        mediaTypeList.add(new MediaType("text", "plain", Charset.forName("UTF-8")));
+        mediaTypeList.add(new MediaType("application", "json", Charset.forName("UTF-8")));
+        mediaTypeList.add(new MediaType("text", "javascript", Charset.forName("UTF-8")));
+
+        stringConverter.setSupportedMediaTypes(mediaTypeList);
+        converters.add(stringConverter);
+
+    }
+
+    /*
+     * Locale bilgisinin cookiler (çerezler) kullanılarak depolanması ve çözümlenmesi için
+     * Springde CookieLocaleResolver beaninin tanımlanaması gerekir.
+     */
+    @Bean
+    public CookieLocaleResolver localeResolver() {
+        CookieLocaleResolver localeResolver = new CookieLocaleResolver();
+        localeResolver.setDefaultLocale(Locale.forLanguageTag("tr-TR"));
+        localeResolver.setCookieName("j4v4cod-locale-cookie");
+        localeResolver.setCookieMaxAge(3600);
+        return localeResolver;
+
+    }
+
+    /*
+     *Oturum locale bilgisinin çözümlenmesi için Spring
+     * SessionLocaleResolver beaninin tanımlanması gerekir.
+     */
+
+    @Bean
+    public SessionLocaleResolver sessionLocaleResolver() {
+        SessionLocaleResolver sessionLocaleResolver = new SessionLocaleResolver();
+        sessionLocaleResolver.setDefaultLocale(Locale.forLanguageTag("tr-TR"));
+        return sessionLocaleResolver;
+
+    }
+
+
+
 }
--- src/main/java/com/j4v4code/controller/HomeController.java
+++ src/main/java/com/j4v4code/controller/HomeController.java
@@ -1,17 +1,21 @@
 package com.j4v4code.controller;
 
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 
 @Controller
 @RequestMapping("/home")
 public class HomeController {
+
+
     @GetMapping("/home")
     public String home(){
-        return "views/home";
+        return "home";
     }
 
 }
