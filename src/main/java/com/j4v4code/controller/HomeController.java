a013f39025c9b7a9ab3595931ebd435e129a9e67
UgurCanGulec <gulecugurcan@gmail.com>
2021-02-11 13:01:03 +0300

    Add comment line in controller

--- src/main/java/com/j4v4code/controller/HomeController.java
+++ src/main/java/com/j4v4code/controller/HomeController.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-
+//Düzenlenecek
 @Controller
 @RequestMapping("/login")
 public class HomeController {
